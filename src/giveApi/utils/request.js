9598f2bd5cecf6819e6a7a0409475ba71e46dac9
Đỗ Hữu Khánh <252460271+dohuukhanh1610@users.noreply.github.com>
2026-09-19 09:19:18 +0700

    Connect to Render API

--- src/giveApi/utils/request.js
+++ src/giveApi/utils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = 'http://localhost:3001/';
+const API_DOMAIN = 'https://quizappapi-0g6w.onrender.com/';
 export const get = async (path) => {
     const res = await fetch(`${API_DOMAIN}${path}`)
     const data = await res.json();
